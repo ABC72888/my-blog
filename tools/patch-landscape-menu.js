26889e88cf20e199ab6a0075e2fbd2fc3438d26b
ABC72888 <ABC72888@users.noreply.github.com>
2026-07-23 13:51:06 +0800

    为文章添加不蒜子浏览统计

--- tools/patch-landscape-menu.js
+++ tools/patch-landscape-menu.js
@@ -1,6 +1,36 @@
 const fs = require('fs');
 const path = require('path');
 
+// 1) Patch article.ejs: add busuanzi counter inside article footer
+const articleEjs = path.join(__dirname, '..', 'node_modules', 'hexo-theme-landscape', 'layout', '_partial', 'article.ejs');
+if (fs.existsSync(articleEjs)) {
+  let code = fs.readFileSync(articleEjs, 'utf8');
+  if (!code.includes('busuanzi_value_page_pv')) {
+    const counter = '<% if (!index){ %> <span style="margin-left:12px;color:#999;font-size:13px"><span class="fa fa-eye"></span> <span id="busuanzi_container_page_pv" style="display:inline;"><span id="busuanzi_value_page_pv"></span> 次浏览</span></span><% } %>';
+    code = code.replace(
+      '<span class="fa fa-share"><%= __(\'share\') %></span>',
+      '<span class="fa fa-share"><%= __(\'share\') %></span>' + counter
+    );
+    fs.writeFileSync(articleEjs, code, 'utf8');
+    console.log('[patch-landscape-menu] busuanzi counter added');
+  } else {
+    console.log('[patch-landscape-menu] busuanzi counter already present');
+  }
+}
+
+// 2) Patch after-footer.ejs: load busuanzi script
+const afterFooter = path.join(__dirname, '..', 'node_modules', 'hexo-theme-landscape', 'layout', '_partial', 'after-footer.ejs');
+if (fs.existsSync(afterFooter)) {
+  let footer = fs.readFileSync(afterFooter, 'utf8');
+  if (!footer.includes('busuanzi.pure.mini')) {
+    footer += '\n<script async src="//busuanzi.ibruce.info/busuanzi/2.3/busuanzi.pure.mini.js"></script>\n';
+    fs.writeFileSync(afterFooter, footer, 'utf8');
+    console.log('[patch-landscape-menu] busuanzi script injected');
+  } else {
+    console.log('[patch-landscape-menu] busuanzi script already present');
+  }
+}
+
 const configPath = path.join(
   __dirname,
   '..',
